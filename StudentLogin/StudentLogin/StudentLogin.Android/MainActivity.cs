57a843f078d51f3ff8a78c1298402e9c3f161135
ConDesktop-32 <ConDesktop-32@DESKTOP-O2SL263>
2019-03-27 10:42:42 +0530

    Track issue in app center

--- StudentLogin/StudentLogin/StudentLogin.Android/MainActivity.cs
+++ StudentLogin/StudentLogin/StudentLogin.Android/MainActivity.cs
@@ -6,6 +6,10 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
 
 namespace StudentLogin.Droid
 {
@@ -20,6 +24,11 @@ namespace StudentLogin.Droid
             base.OnCreate(bundle);
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
+            AppCenter.Start("68bd10fa-297b-4365-a1cf-abee6a305e45",
+                   typeof(Analytics), typeof(Crashes));
+            AppCenter.Start("68bd10fa-297b-4365-a1cf-abee6a305e45", typeof(Analytics), typeof(Crashes));
+            Crashes.GenerateTestCrash();
+          
             LoadApplication(new App());
         }
     }
--- StudentLogin/StudentLogin/StudentLogin/MainPage.xaml.cs
+++ StudentLogin/StudentLogin/StudentLogin/MainPage.xaml.cs
@@ -1,8 +1,6 @@
-﻿using System;
+﻿using Microsoft.AppCenter.Crashes;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace StudentLogin
@@ -12,6 +10,8 @@ namespace StudentLogin
         public MainPage()
         {
             InitializeComponent();
+            var properties = new Dictionary<string, string> { { "Appcenter", "TrackIssue" } };
+            Crashes.TrackError(new Exception(), properties);
         }
     }
 }
